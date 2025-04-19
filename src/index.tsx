87060c88978a620a32aa565583ea7be5673af0ea
Hong-JunHyeok <edb1631@naver.com>
2025-04-19 15:22:38 +0900

    초기 환경 설정

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BaseStyles, ThemeProvider } from "@primer/react";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <BaseStyles>
+        <App />
+      </BaseStyles>
+    </ThemeProvider>
+  </StrictMode>
+);
