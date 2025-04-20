544a56a238103cb8d33999a31ce03468465042ca
Hong-JunHyeok <edb1631@naver.com>
2025-04-20 14:01:28 +0900

    locatorjs 도입

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@primer/react";
 import { RouterProvider } from "@tanstack/react-router";
+import setupLocatorUI from "@locator/runtime";
 import router from "./Router";
 
 import "./index.css";
+
+if (import.meta.env.NODE_ENV === "development") {
+  setupLocatorUI();
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
